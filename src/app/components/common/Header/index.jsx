30c0ebfa8b6939515a3a2f59989c9cf8ebfffc1e
Ronny <ronny.rook@labela.nl>
2018-10-24 21:18:27 +0200

    Add Container to center elements

--- src/app/components/common/Header/index.jsx
+++ src/app/components/common/Header/index.jsx
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import PT from 'prop-types';
 
 import Container from 'common/Container';
 import { Navigation, Name, Letter } from './styled';
--- src/app/components/modules/Home/index.jsx
+++ src/app/components/modules/Home/index.jsx
@@ -3,9 +3,16 @@ import PT from 'prop-types';
 import styled from 'styled-components';
 
 import Header from 'common/Header';
+import Container from 'common/Container';
 
 const Home = () => (
-  <Header />
+  <>
+    <Header />
+    <Container>
+      <p>Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+      <p>Nulla vitae elit libero, a pharetra augue. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    </Container>
+  </>
 );
 
 export default Home;

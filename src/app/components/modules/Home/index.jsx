f86229596eac7a0effccf2bbe3babfafd9638fa3
Ronny <ronny.rook@labela.nl>
2018-10-25 09:49:10 +0200

    Add React 16.6 with Suspense loading and add multiple routes with Cv

--- src/app/components/modules/Home/index.jsx
+++ src/app/components/modules/Home/index.jsx
@@ -1,53 +1,22 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Header from 'common/Header';
 import Container from 'common/Container';
 import Cards from 'common/Cards';
-import Title from 'common/Title';
 import { IntroTitle } from './styled';
 
-class Home extends PureComponent {
-  state = {
-    scrolledPage: false,
-  };
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll = () => {
-    const scrollY = window.scrollY;
-
-    if (scrollY >= 10 && !this.state.scrolledPage) {
-      this.setState({ scrolledPage: true });
-      document.body.classList.add('scrolled');
-    } else if (scrollY < 10 && this.state.scrolledPage) {
-      this.setState({ scrolledPage: false });
-      document.body.classList.remove('scrolled');
-    }
-  }
-
-  render() {
-    const { scrolledPage } = this.state;
-
-    return (
-      <>
-        <Header scrolledPage={scrolledPage} />
-        <Container>
-          <IntroTitle center>Hi, I'm Ronny</IntroTitle>
-          <p>Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
-        </Container>
-        <Cards />
-        <Container>
-          <p>Sed posuere consectetur est at lobortis. Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-        </Container>
-      </>
-    );
-  }
-}
+const Home = () => (
+  <>
+    <Container>
+      <IntroTitle center>Hi, I'm Ronny</IntroTitle>
+      <p>Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+    </Container>
+    <Cards />
+    <Container>
+      <p>Sed posuere consectetur est at lobortis. Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+      <Link to="/cv">Go to CV</Link>
+    </Container>
+  </>
+);
 
 export default Home;

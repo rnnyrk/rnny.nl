f4a084053197c7c38ae038c97ecdc872b4309edd
Ronny <ronny.rook@labela.nl>
2019-02-04 13:27:43 +0100

    Add wave to icon in Bio

--- src/components/modules/About/components/Bio/index.jsx
+++ src/components/modules/About/components/Bio/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { BioContainer, BioHeader, Photo, Title } from './styled';
+import { BioContainer, BioHeader, Photo, Title, Wave } from './styled';
 
 const Bio = () => (
   <BioContainer>
     <BioHeader>
       <Photo />
-      <Title>Hi, I’m Ronny! 👋</Title>
+      <Title>Hi, I’m Ronny! <Wave>👋</Wave></Title>
     </BioHeader>
     <p>I’m a JavaScript (mostly front-end) developer from Amsterdam, the Netherlands 🇳🇱. I’ve got a passion for design, kitesurfing, running, (oldskool) hiphop, (web) performance and - cutting-egde - web technologies such as React (Native), Progressive Web Apps, Styled Components and CSS Grid.</p>
   </BioContainer>
--- src/components/modules/About/components/Bio/styled.js
+++ src/components/modules/About/components/Bio/styled.js
@@ -1,15 +1,5 @@
-import styled from 'styled-components';
-
-export const BioContainer = styled.div`
-  grid-column: 4 / span 6;
-`;
-
-export const BioHeader = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin-bottom: 25px;
-`;
+import styled, { keyframes } from 'styled-components';
+import { media } from 'styles/utils';
 
 export const Photo = styled.figure`
   width: 80px;
@@ -22,4 +12,31 @@ export const Photo = styled.figure`
 export const Title = styled.h2`
   font-size: 28px;
   font-weight: 700;
+`;
+
+export const Wave = styled.span`
+  display: inline-block;
+`;
+
+const rotate = keyframes`
+  0% { transform: rotate(0deg); }
+  50% { transform: rotate(20deg); }
+  100% { transform: rotate(0deg); }
+`;
+
+export const BioHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-bottom: 25px;
+
+  ${media.desktop`
+    &:hover ${Wave} {
+      animation: ${rotate} .5s linear infinite;
+    }
+  `}
+`;
+
+export const BioContainer = styled.div`
+  grid-column: 4 / span 6;
 `;

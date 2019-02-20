6cb2c6125e270ae4e1607ddd6c666178ca3e1f97
Ronny Rook <info@ronnyrook.nl>
2019-02-20 22:38:17 +0100

    Change indenting of props in Dialog

--- src/components/common/Dialog/index.tsx
+++ src/components/common/Dialog/index.tsx
@@ -4,9 +4,9 @@ import { PoseGroup } from 'react-pose';
 
 import { DialogOverlay, DialogBody } from './styled';
 
-const Dialog:FC<DialogProps> = (
-  { isOpen, onClose, children }
-) => createPortal(
+const Dialog:FC<DialogProps> = ({
+  isOpen, onClose, children
+}) => createPortal(
   <PoseGroup>
     {isOpen && [
       <DialogBody key="modal-body">{children}</DialogBody>,

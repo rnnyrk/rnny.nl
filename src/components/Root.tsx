eeb742e5aa8deb228554b9733ab7d8a1d886f88e
Ronny Rook <info@ronnyrook.nl>
2019-02-28 22:04:52 +0100

    Remove Redux

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import store from 'store';
 import theme from 'styles/theme';
 import App from './App';
 
 const Root = () => (
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 export default Root;
